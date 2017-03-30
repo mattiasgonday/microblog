a3fc07fa395a6fd1beacb38024ab4687e3084cc3
Mattias Gonday <mattias.gonday@afconsult.com>
2017-03-30 12:24:10 +0200

    User Logins

--- app/models.py
+++ app/models.py
@@ -5,7 +5,25 @@ class User(db.Model):
     nickname = db.Column(db.String(64), index=True, unique=True)
     email    = db.Column(db.String(128), index=True, unique=True)
     posts    = db.relationship('Post', backref='author', lazy='dynamic')
-    
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id) # python 2
+        except NameError:
+            return str(self.id) # python 3
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
@@ -17,4 +35,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
-                        
